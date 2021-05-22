154b2486e48907e5e00007749d352e0825cc00be
ToOcMGit <to_oc_m_git@outlook.be>
2021-05-22 19:28:29 +0200

    Added GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Reader from './components/reader/Reader';
+import { GlobalStyle } from './components/theme/GlobalStyles';
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Reader title="Title" text="Lorem ipsum sit amet"/>
     </>
   );
